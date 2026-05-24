aec134f2180fa80c402dc6a70332cf930ea54693
Mahak Singhal <rkmsinghal19@gmail.com>
2026-05-24 11:05:46 +0530

    new

---
+++ 8/1.js
@@ -0,0 +1,10 @@
+const btn1 = document.querySelector(".btn_1");
+const btn2 = document.querySelector(".btn_2");
+const btn3 = document.querySelector(".btn_3");
+
+btn1.addEventListener("click", () => {
+    alert("Clicked!");
+});
+btn2.addEventListener("dblclick", () => {
+    alert("Double clicked!");
+});
